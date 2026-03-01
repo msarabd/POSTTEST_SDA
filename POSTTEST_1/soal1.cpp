8cfc55ab2b72f4f95d3b1812a57c3e04730c7d9e
Mahdi <msar.abdullah.idn@gmail.com>
2026-03-01 22:55:12 +0800

    last bang

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int FindMin(int arr[], int n){
     int min = arr[0];
 
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i < n; i++){
         if (arr[i] < min){
             min = arr[i];
         }
